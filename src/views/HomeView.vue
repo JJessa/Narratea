c695baca78804c79893aacf5ecf420c31e24f782
JJessa <jessicamosteiro13@gmail.com>
2023-06-07 17:54:36 +0200

    creados div parent y topChild div

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
 
  <NavBar />
 
-  <div>
-
+ <div class="parent_div">
+      <div class="top_child_div"></div>
   </div>
 
  <Footer />
@@ -14,3 +14,22 @@
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/Footer.vue'
 </script>
+
+<style scoped>
+.parent_div {
+  width: 90vw;
+  height: 85vh;
+  background-color: #f55353;
+  position: relative;
+  margin: 0 auto;
+}
+.top_child_div {
+  width: 50%;
+  height: 30%;
+  background-color: #feb139;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
