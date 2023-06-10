92307a20a09b1a7bb93224d8897b62e9f935ea41
JJessa <jessicamosteiro13@gmail.com>
2023-06-10 19:17:51 +0200

    creado titulo y divs pequeños de la home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,29 @@
  <NavBar />
 
  <div class="parent_div">
-      <div class="top_child_div"></div>
+  <div class="home_title">
+    <h1 >Narratea</h1>
+  </div>
+  <div class="top_child_div">
+      <p class="home_parraf">Escribe, publica y comparte tus relatos cortos con otros escritores y lectores apasionados por la literatura</p> 
+      <p class="home_parraf">¡Da vida a tus historias y deja que tus palabras vuelen!</p> 
+  </div>
+  <div class="total_children_div">
+    <div class="about_us children_divs">
+      <div>
+        <img />
+        <h4>Sobre Nosotros</h4>
+      </div>
+      <p> Conoce más acerca de quiénes somos, y cómo trabajamos para crear una plataforma de calidad</p>
+    </div>
+    <div class="stories children_divs">
+      <div>
+        <img />
+        <h4>Relatos</h4>
+      </div>
+      <p> Déjate llevar por la imaginación y descubre nuevas historias explorando nuestros relatos cortos</p>
+    </div>
+  </div>
   </div>
 
  <Footer />
@@ -25,15 +47,42 @@ import Footer from '../components/Footer.vue'
   background-position: center;
   position: relative;
   margin: 0 auto;
-  /* background-color: #DADADA; */
 }
 .top_child_div {
-  width: 50%;
-  height: 30%;
-  background-color: #feb139;
+  width: 60%;
+  height: 20%;
+  backdrop-filter: blur(10px);
   position: absolute;
-  top: 20%;
+  top: 35%;
   left: 50%;
   transform: translate(-50%, -50%);
+  
+}
+
+.home_title {
+  font-size: 4rem; 
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.home_parraf {
+  text-align: center;
+  font-size: 1.5rem;
+}
+
+.total_children_div {
+  width: 77%;
+  display: flex;
+  justify-content: space-around;
+  top: 70%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position:absolute;
+}
+.children_divs {
+  width: 25%;
+  height: 25%;
+  backdrop-filter: blur(10px);
 }
 </style>
