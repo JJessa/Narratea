56d476e10e0e4aea6538b90e13b62c5cb33a5040
JJessa <jessicamosteiro13@gmail.com>
2023-06-11 13:59:18 +0200

    añadidos navbar y footer a AboutUsView

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,6 +1,13 @@
 <template>
+  <NavBar />
     <div>
     <h1>About Us</h1>
     <router-link to="/">Home</router-link>
    </div>
-     </template>
+  <Footer />
+</template>
+
+<script setup>
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
+</script>
